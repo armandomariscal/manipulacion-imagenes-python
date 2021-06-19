9066d6815233906aaa90d2af4fe3a03b64a722bd
Armando Mariscal <armando.marisscal@gmail.com>
2021-06-18 22:56:39 -0600

    Cortar imagen

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from PIL import Image
 
 if __name__ == '__main__':
-    image = Image.open('images/')
+
+	try:
+		wallpaper = Image.open('images/wallpaper.jpg')
+		wp = wallpaper.crop(
+			(2500, 1500, 3400, 2180) # LURB
+		)
+		wp.save('images/wp.jpg')
+	except FileNotFoundError as e:
+		print('No es posible obtener la imagen.')
